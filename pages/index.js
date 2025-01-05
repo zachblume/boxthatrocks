93b5ce68de03e94cc67c19f96273d7077549bfa7
Zach Blume <zachblume@gmail.com>
2025-01-04 19:37:55 -0500

    supabase not entirely working yet

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
+import { supabase } from "@/lib/suapbaseClient";
+import { Auth } from "@supabase/auth-ui-react";
+
 export default function Home() {
+    const isLoggedIn = supabase.auth.getUser();
     return (
         <div className="text-center my-6">
             <h1 className="text-3xl font-extrabold">Box that rocks</h1>
@@ -6,6 +10,10 @@ export default function Home() {
                 The best platform to run your monthly subscription box
                 e-commerce store
             </p>
+            {JSON.stringify(isLoggedIn)}
+            <div className="m-10 border border-gray-300 rounded-md p-6 shadow-md">
+                <Auth supabaseClient={supabase} magicLink />
+            </div>
         </div>
     );
 }
