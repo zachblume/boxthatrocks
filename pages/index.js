b90322c0c8941b981d7ea430d51ab93c1dea37e5
Zach Blume <zachblume@gmail.com>
2025-01-04 20:47:24 -0500

    store naming working

--- pages/index.js
+++ pages/index.js
@@ -61,15 +61,33 @@ const SetNameOfStoreForm = () => {
     };
 
     return (
-        <form onSubmit={updateStoreName}>
-            <label htmlFor="storeName">Store Name</label>
+        <form
+            onSubmit={updateStoreName}
+            className="flex gap-5 max-w-lg mx-auto whitespace-nowrap my-6"
+        >
+            <div className="my-auto">
+                <label
+                    htmlFor="storeName"
+                    className="block text-sm font-medium text-gray-700"
+                >
+                    Store Name
+                </label>
+            </div>
             <input
                 type="text"
                 id="storeName"
                 name="storeName"
                 defaultValue={storeName}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
-            <button type="submit">Update</button>
+            <div>
+                <button
+                    type="submit"
+                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    Update
+                </button>
+            </div>
         </form>
     );
 };
