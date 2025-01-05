9f504e6d5f9ca763a363ee85a7782ad965f1e6b2
Zach Blume <zachblume@gmail.com>
2025-01-04 20:13:31 -0500

    supabase auth now working

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
 import { supabase } from "@/lib/suapbaseClient";
 import { Auth } from "@supabase/auth-ui-react";
+import { useEffect, useState } from "react";
+
+const useUser = () => {
+    const [user, setUser] = useState(supabase.auth.getUser());
+    useEffect(() => {
+        const { data: authListener } = supabase.auth.onAuthStateChange(
+            async (event, session) => {
+                const currentUser = session?.user;
+                setUser(currentUser ?? null);
+            }
+        );
+        return () => {};
+    });
+    return user;
+};
 
 export default function Home() {
-    const isLoggedIn = supabase.auth.getUser();
+    const user = useUser();
     return (
         <div className="text-center my-6">
             <h1 className="text-3xl font-extrabold">Box that rocks</h1>
@@ -10,10 +25,14 @@ export default function Home() {
                 The best platform to run your monthly subscription box
                 e-commerce store
             </p>
-            {JSON.stringify(isLoggedIn)}
-            <div className="m-10 border border-gray-300 rounded-md p-6 shadow-md">
-                <Auth supabaseClient={supabase} magicLink />
-            </div>
+            {!user?.id && <Login />}
+            {user?.id && "Logged in!"}
         </div>
     );
 }
+
+const Login = () => (
+    <div className="m-10 border border-gray-300 rounded-md p-6 shadow-md">
+        <Auth supabaseClient={supabase} magicLink />
+    </div>
+);
